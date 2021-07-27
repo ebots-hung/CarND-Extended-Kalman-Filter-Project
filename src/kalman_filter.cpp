d05cd36683bac09510791dfa3d4a64466442aef1
ebots-hung <hlam@ebots.com>
2021-07-27 10:11:15 -0700

    update kalman filter results

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
 
+#define PI 3.14159265
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -26,16 +28,57 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_*x_;
+  MatrixXd l_Ft_m = F_.transpose();
+  P_ = F_*P_*l_Ft_m + Q_; 
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd l_zp_v = H_ * x_;
+  VectorXd l_y_v = z - l_zp_v;
+  MatrixXd l_Ht_m = H_.transpose();
+  MatrixXd l_S_m = H_ * P_ * l_Ht_m + R_;
+  MatrixXd l_Si_m = l_S_m.inverse();
+  MatrixXd l_PHt_m = P_ * l_Ht_m;
+  MatrixXd l_K_m = l_PHt_m * l_Si_m;
+
+  // New estimtes
+  x_ = x_ + (l_K_m * l_y_v);
+  long l_statesize_l = x_.size();
+  MatrixXd l_I_m = MatrixXd::Identity(l_statesize_l, l_statesize_l);
+  P_ = (l_I_m - (l_K_m * H_)) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+
+  float l_rho_f = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  float l_phi_f = atan2(x_(1), x_(0));
+  float l_rhodot_f = (fabs(l_rho_f) < 0.0001) ? (0): ((x_(0)*x_(2) + x_(1)*x_(3))/l_rho_f);
+  VectorXd l_zp_v(3);
+  l_zp_v << l_rho_f, l_phi_f, l_rhodot_f;
+  VectorXd l_y_v = z - l_zp_v;
+
+  if( l_y_v[1] > PI )
+    l_y_v[1] -= 2.f*PI;
+  if( l_y_v[1] < -PI )
+    l_y_v[1] += 2.f*PI;
+
+  Tools l_tools;
+  MatrixXd l_Hj_m = l_tools.CalculateJacobian( x_ );
+  MatrixXd l_Hjt_m = l_Hj_m.transpose();
+  MatrixXd l_S_m = l_Hj_m * P_ * l_Hjt_m + R_;
+  MatrixXd l_Si_m = l_S_m.inverse();
+  MatrixXd l_K_m = P_ * l_Hjt_m * l_Si_m;
+
+  // New estimtes
+  x_ = x_ + (l_K_m * l_y_v);
+  long state_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(state_size, state_size);
+  P_ = (I - (l_K_m * H_)) * P_;  
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,20 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+   VectorXd l_rmse_v(4);
+   l_rmse_v << 0, 0, 0, 0;
+   
+   // Getting the squared difference
+   for (unsigned int i=0;i < estimations.size(); ++i){
+      VectorXd l_residual_v = estimations[i] - ground_truth[i];
+      l_residual_v = l_residual_v.array()*l_residual_v.array();
+      l_rmse_v += l_residual_v;
+   }
+   
+   // taking the average value for rmse
+   l_rmse_v /= estimations.size();
+   l_rmse_v = l_rmse_v.array().sqrt();
+   return l_rmse_v;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +35,24 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  MatrixXd l_Hj_m(3, 4);
+  // state variables
+  float l_px_f = x_state(0);
+  float l_py_f = x_state(1);
+  float l_vx_f = x_state(2);
+  float l_vy_f = x_state(3);
+
+  float l_c1_f = l_px_f*l_px_f + l_py_f*l_py_f;    // l_c1_f = l_px_f^2 + l_py_f^2
+  float l_c2_f = sqrt(l_c1_f);                     // l_c2_f = sqrt(l_px_f^2 + l_py_f^2)
+  float l_c3_f = (l_c1_f*l_c2_f);                  // l_c3_f = (l_px_f^2 + l_py_f^2)^(3/2)
+
+  if (fabs(l_c1_f) < 0.0001){
+    std::cout << "Division by Zero!" << std::endl;
+    return l_Hj_m;
+  }
+  l_Hj_m << (l_px_f/l_c2_f), (l_py_f/l_c2_f), 0, 0,
+        -(l_py_f/l_c1_f), (l_px_f/l_c1_f), 0, 0,
+        l_py_f*(l_vx_f*l_py_f - l_vy_f*l_px_f) / l_c3_f, l_px_f*(l_px_f*l_vy_f - l_py_f*l_vx_f) / l_c3_f, l_px_f / l_c2_f, l_py_f / l_c2_f;
+  
+  return l_Hj_m;  
 }
